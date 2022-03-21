602629487377d855564f8659ce959f659ba1122f
Edwin R <edwin0923@gmail.com>
2022-03-20 22:18:26 -0500

    Update for deployment

---
+++ components/layouts/Layout.tsx
@@ -0,0 +1,38 @@
+import { FC } from 'react'
+
+import Head from 'next/head'
+
+import { Navbar } from '../ui';
+
+interface Props {
+  title?: string
+}
+
+const origin = (typeof window === 'undefined') ? '' : window.location.origin
+
+export const Layout: FC<Props> = ({children, title}) => {
+// export const Layout = ({children, title}: props ) => {
+
+  return (
+    <>
+      <Head>
+        <title>{ title || 'Pokemon App' }</title>
+        <meta name="author" content="Edwin R." />
+        <meta name="description" content={`Información sobre el pokemón ${title}`} />
+        <meta name="keywords" content={`${title}, pokemon, pokedex`} />
+
+        <meta property="og:title" content={`Información sobre ${title}`} />
+        <meta property="og:description" content={`Esta es la página sobre ${title}`} />
+        <meta property="og:image" content={`${origin}/img/banner.png`} />
+      </Head>
+
+      <Navbar />
+
+      <main style={{
+        padding: '0px 20px'
+      }}>
+        {children}
+      </main>
+    </>
+  )
+}
---
+++ components/ui/Navbar.tsx
@@ -0,0 +1,43 @@
+import NextLink from 'next/link'
+import Image from 'next/image'
+
+import { Link, Spacer, Text, useTheme } from '@nextui-org/react'
+
+export const Navbar = () => {
+
+  const { theme } = useTheme()
+
+  return (
+    <div style={{
+      display: 'flex',
+      width: '100%',
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'start',
+      padding: '0px 20px',
+      backgroundColor: theme?.colors.gray900.value
+    }}>
+      <Image
+        src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png"
+        alt="Icono de la app"
+        width={70}
+        height={70}
+      />
+
+      <NextLink href="/" passHref>
+        <Link>
+          <Text color="white" h2>P</Text>
+          <Text color="white" h3>okemon</Text>
+        </Link>
+      </NextLink>
+
+      <Spacer css={{ flex: 1 }} />
+
+      <NextLink href="/favorites" passHref>
+        <Link>
+          <Text color="white">Favoritos</Text>
+        </Link>
+      </NextLink>
+    </div>
+  )
+}
